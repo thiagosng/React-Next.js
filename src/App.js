e98ec472af869bdae1d625d39529ffcc1cc4ff00
thiagosng <thiagonunes026@gmail.com>
2021-05-24 16:54:43 -0300

    Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    state = {
+      counter: 0,
+     posts: [
+       {
+         id: 1,
+         title: 'titulo 1',
+         body: 'corpo 1'
+       },
+       {
+        id: 1,
+        title: 'titulo 2',
+        body: 'corpo 2'
+      },
+      {
+        id: 1,
+        title: 'titulo 3',
+        body: 'corpo 3'
+      }
+     ]
+    };
+
+    timeoutUpdate = null;
+
+    componentDidMount() {
+     this.handleTimeout();
+  }
+    componentDidUpdate() {
+     this.handleTimeout();
+    }
+    componentWillUnmount (){
+      clearTimeout(this.timeoutUpdate);
+    }
+
+    handleTimeout = () => {
+      const { posts, counter } = this.state;
+      posts[0].title = 'O titulo mudou';
+      
+      this.timeoutUpdate = setTimeout(() => {
+          this.setState({ posts, counter: counter + 1 });
+    }, 1000);
+    } 
+
+  render(){
+    const { posts, counter } = this.state;
+
+    return (
+      <div className="App">
+          <h1>{counter}</h1>
+          {posts.map(post =>(
+            <div key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+            </div>
+          ))}
+      </div>
+  )
+ }
 }
-
 export default App;
