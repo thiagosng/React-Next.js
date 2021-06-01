c9ce38170287d14a8d2530801db5005af7d75f29
thiagosng <thiagonunes026@gmail.com>
2021-06-01 10:54:14 -0300

    Atualizando os testes

--- src/Components/Posts/Posts.spec.jsx
+++ src/Components/Posts/Posts.spec.jsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import { Posts } from '../Posts';
+import { Posts } from '.';
 
 
 const props = {
@@ -30,20 +30,26 @@ describe('<Posts />', () => {
     it('should render posts', () => {
         render(<Posts {...props} />);
 
-        expect(screen.getAllByRole('heading', { name: /title/i}))
+        expect(screen.getAllByRole('heading', { name: /title/i }))
         .toHaveLength(3);
-        expect(screen.getAllByRole('img', { name: /title/i}))
+        expect(screen.getAllByRole('img', { name: /title/i }))
         .toHaveLength(3);
         expect(screen.getAllByText(/body/i))
         .toHaveLength(3);
-        expect(screen.getByRole('img', { name: /title 3/i}))
+        expect(screen.getByRole('img', { name: /title 3/i }))
         .toHaveAttribute('src', 'img/img3.png');
+    });
+
+    it('should not render posts', () => {
+        render(<Posts />);
+        expect(screen.queryByRole('heading', { name: /title/i }))
+        .not.toBeInTheDocument();
     });
 
     it('should match snapshot', () => {
         const {container} = render(<Posts {...props} />);
-
-        expect(container.firstChild).toMatchSnapshot();
-    
+        expect(container.firstChild).toMatchSnapshot(); 
     });
-})
+
+    
+});
