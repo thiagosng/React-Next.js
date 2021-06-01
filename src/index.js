1e8ddbec44f2c4be42f5cc31c52b0717fb508157
thiagosng <thiagonunes026@gmail.com>
2021-06-01 09:29:53 -0300

    Implementando testes

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import {Home} from './templates/Home';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <Home numberToIncrement = {2} />
   </React.StrictMode>,
   document.getElementById('root')
 );
