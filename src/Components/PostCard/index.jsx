d580d7555a6eff6881cc735965ba310189053dd2
thiagosng <thiagonunes026@gmail.com>
2021-06-01 11:59:36 -0300

    Atualizando os testes e configurando ESLint

--- src/Components/PostCard/index.jsx
+++ src/Components/PostCard/index.jsx
@@ -1,12 +1,22 @@
+import P from 'prop-types';
 import './styles.css';
 
-export const PostCard = ({title,body,id,cover}) => (
-    
-        <div className="post">
-            <img src={cover} alt={title} />
-            <div key={id} className="post-content">
-                <h2>{title} {id}</h2>
-                <p>{body}</p>
-            </div>
-        </div>
-    );
+export const PostCard = ({ title, body, id, cover }) => (
+  <div className="post">
+    <img src={cover} alt={title} />
+    <div key={id} className="post-content">
+      <h2>
+        {title} {id}
+      </h2>
+      <p>{body}</p>
+    </div>
+  </div>
+);
+
+
+PostCard.propTypes = {
+  title: P.string.isRequired,
+  body: P.string.isRequired,
+  id: 
+};
+
